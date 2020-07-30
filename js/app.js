1b1578ab927bcdfcab9b2dd7038c1434b21d1d9d
Jelson J <jelson20outlook.com>
2020-07-30 17:14:18 +0100

    Commint

---
+++ js/app.js
@@ -0,0 +1,77 @@
+'use stric';
+class Person {
+	static species() {
+		return 'pipipire';
+	}
+
+	static specieSent() {
+		return `I love JavaScript with ${species()}`;
+	}
+
+	constructor(fName, lName, gender, salary) {
+		this.fName = fName;
+		this.lName = lName;
+		this.gender = gender;
+		this.salary = salary;
+		this.hasJob = false;
+	}
+
+	fullName() {
+		return `${this.fName} ${this.lName}`;
+	}
+
+	setFirstName(fName) {
+		this.fName = fName;
+	}
+	setLastName(lName) {
+		this.lName = lName;
+	}
+	setGender(gender) {
+		this.gender = gender;
+	}
+	setSalary(salary) {
+		this.salary = salary;
+	}
+}
+
+class Employee extends Person {
+	constructor(fName, lName, job, gender, salary) {
+		super(fName, lName, gender, salary);
+		this.job = job;
+		this.hasJob = true;
+	}
+	setJob(job) {
+		this.job = job;
+	}
+	setGender(gender) {
+		this.gender = gender;
+	}
+	setSalary(salary) {
+		this.salary = salary;
+	}
+}
+let x,
+	text = '';
+const person = new Person('Julia', 'Emma', 'Female', 1768, 'false');
+
+// displaying the Object in a Loop
+for (x in person) {
+	text += person[x] + ' ';
+}
+
+//Using Object.values
+const employee = new Employee(
+	'Smith',
+	'Bento',
+	'Software Engineer',
+	'Male',
+	2502
+);
+let myArray = Object.values(employee);
+
+document.querySelector('#app').innerHTML = text;
+
+//document.getElementById('app').innerHTML = myArray;
+
+console.log(employee);
+console.log(person);
